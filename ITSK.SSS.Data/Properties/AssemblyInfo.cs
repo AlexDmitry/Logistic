edc521087ed3d8cf1e9eeff55481cb517efe2cf0
AlexDmitry <aleksdmitry@gmail.com>
2018-08-13 11:47:17 +0300

    6

---
+++ ITSK.SSS.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ITSK.SSS.Data")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("244e430b-5ad7-4b32-8f77-40ca106fa81e")]
+
